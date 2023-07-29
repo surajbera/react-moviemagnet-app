ad5326dc8e51503fab458b1fa8887e978751eb67
suraj.be <suraj.be@media.net>
2023-07-29 11:42:31 +0530

    Added dark mode in the app

--- src/components/page-layout/PageLayout.jsx
+++ src/components/page-layout/PageLayout.jsx
@@ -7,7 +7,7 @@ import PageTitle from '../page-title/PageTitle';
 
 const PageLayout = () => {
   return (
-    <div>
+    <div className='dark:bg-gray-900 dark:text-gray-300 root-wrapper'>
       <Navbar />
       <PageTitle title='Box Office' subTitle='Are you looking for a movie or an actor?' />
       <Outlet />
--- src/components/search-form/SearchForm.jsx
+++ src/components/search-form/SearchForm.jsx
@@ -77,7 +77,7 @@ const SearchForm = ({
             value='shows'
             name='search-option'
             onChange={handleSearchOptionChange}
-            className='w-4 h-4 text-yellow-400 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-yellow-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+            className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
           />
           <label
             htmlFor='default-radio-1'
@@ -94,7 +94,7 @@ const SearchForm = ({
             value='peoples'
             name='search-option'
             onChange={handleSearchOptionChange}
-            className='w-4 h-4 text-yellow-400 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-yellow-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+            className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
           />
           <label
             htmlFor='default-radio-2'
