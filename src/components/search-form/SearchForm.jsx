3b9966d360b56cf4fff6c0723136c525516f79c0
suraj.be <suraj.be@media.net>
2023-08-01 19:19:39 +0530

    Styled radio buttons

--- src/components/search-form/SearchForm.jsx
+++ src/components/search-form/SearchForm.jsx
@@ -81,42 +81,46 @@ const SearchForm = ({
         )}
       </div>
       {/* RADIO BUTTON CONTAINER */}
-      <div>
-        <div className='flex items-center mb-4'>
-          <input
-            checked={searchOption === 'shows' ? true : false}
-            id='default-radio-1'
-            type='radio'
-            value='shows'
-            name='search-option'
-            onChange={handleSearchOptionChange}
-            className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-          />
-          <label
-            htmlFor='default-radio-1'
-            className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-          >
-            Shows
-          </label>
-        </div>
-        <div className='flex items-center'>
-          <input
-            checked={searchOption === 'peoples' ? true : false}
-            id='default-radio-2'
-            type='radio'
-            value='peoples'
-            name='search-option'
-            onChange={handleSearchOptionChange}
-            className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-          />
-          <label
-            htmlFor='default-radio-2'
-            className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-          >
-            Peoples
-          </label>
-        </div>
-      </div>
+      <ul className='items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg flex dark:bg-gray-700 dark:border-gray-600 dark:text-white my-2 max-w-[400px] mx-auto'>
+        <li className='w-full border-b border-gray-200 border-b-0 border-r dark:border-gray-600'>
+          <div className='flex items-center pl-3'>
+            <input
+              checked={searchOption === 'shows' ? true : false}
+              id='default-radio-1'
+              type='radio'
+              value='shows'
+              name='search-option'
+              onChange={handleSearchOptionChange}
+              className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500'
+            />
+            <label
+              htmlFor='default-radio-1'
+              className='w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+            >
+              Shows
+            </label>
+          </div>
+        </li>
+        <li className='w-full dark:border-gray-600'>
+          <div className='flex items-center pl-3'>
+            <input
+              checked={searchOption === 'peoples' ? true : false}
+              id='default-radio-2'
+              type='radio'
+              value='peoples'
+              name='search-option'
+              onChange={handleSearchOptionChange}
+              className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500'
+            />
+            <label
+              htmlFor='default-radio-2'
+              className='w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+            >
+              Peoples
+            </label>
+          </div>
+        </li>
+      </ul>
     </form>
   );
 };
