ba9bec5ce902d59a611fd08530dc8e7aaa0aeada
suraj.be <suraj.be@media.net>
2023-08-01 12:30:33 +0530

    Installed axios package

--- src/services/tvmazeService.js
+++ src/services/tvmazeService.js
@@ -1,15 +1,20 @@
+import axios from 'axios';
 import { customConsoleLog } from '../utils/customConsoleLog';
 import { customDelay } from '../utils/customDelay';
 
 const BASE_URL = 'https://api.tvmaze.com';
 
 export const apiGet = async (queryString) => {
-  // throw new Error('Something bad happened');
-  customConsoleLog(`${BASE_URL}${queryString}`, '#15803d');
-  const response = await fetch(`${BASE_URL}${queryString}`);
-  const data = await response.json();
-  await customDelay(1500);
-  return data;
+  try {
+    customConsoleLog(`${BASE_URL}${queryString}`, '#0891b2');
+    const response = await axios.get(`${BASE_URL}${queryString}`);
+    await customDelay(1500);
+    return response.data;
+  } catch (error) {
+    // You can log or handle the error here, or throw it to be caught by a higher-level error handler
+    console.error(`Error fetching data from ${BASE_URL}${queryString}:`, error);
+    throw error; // Optional: re-throw the error if you want calling code to handle it
+  }
 };
 
 export const getShowsByQuery = (query) => apiGet(`/search/shows?q=${query}`);
