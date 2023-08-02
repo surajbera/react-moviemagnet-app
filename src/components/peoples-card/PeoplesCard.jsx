a39e2c4a13e7f2321d3d49f6178d3fa6a201833b
suraj.be <suraj.be@media.net>
2023-08-02 16:02:42 +0530

    Updated Peoples Grid

--- src/components/peoples-card/PeoplesCard.jsx
+++ src/components/peoples-card/PeoplesCard.jsx
@@ -2,19 +2,22 @@ import NotFoundImg from '../../assets/not-found.jpg';
 
 const PeoplesCard = ({ data }) => {
   return (
-    <div className='border-2 border-cyan-500 border-solid mb-10'>
-      <img
-        src={data.image?.medium ? data.image.medium : NotFoundImg}
-        alt={data.name}
-        className='mb-2'
-      />
-      <p className='border-2 border-cyan-600 border-solid mb-2'>
+    <div className='border-2 border-solid border-gray-300 rounded-lg p-2 dark:border-none dark:p-0'>
+      <div className='w-[200px] mx-auto mb-2 h-[300px] max-w-full'>
+        <img
+          src={data.image?.medium ? data.image.medium : NotFoundImg}
+          alt={data.name}
+          className='rounded-xl w-full h-full object-cover block'
+        />
+      </div>
+
+      <p className='mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white'>
         {data.name} {data.gender && `(${data.gender})`}
       </p>
-      <p className='border-2 border-cyan-600 border-solid'>
+      <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
         {data.country ? `Comes from ${data.country.name}` : 'Country Not Known'}
       </p>
-      <p className='border-2 border-cyan-600 border-solid'>
+      <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
         {data.birthday ? `Born on ${data.birthday}` : 'Birthday Not Known'}
       </p>
     </div>
--- src/components/peoples-grid/PeoplesGrid.jsx
+++ src/components/peoples-grid/PeoplesGrid.jsx
@@ -2,6 +2,16 @@ import PeoplesCard from '../peoples-card/PeoplesCard';
 
 const PeoplesGrid = ({ peoplesData }) => {
   if (peoplesData.length === 0) return 'No Results!!!';
-  return peoplesData.map((item) => <PeoplesCard key={item.person.id} data={item.person} />);
+  // return peoplesData.map((item) => <PeoplesCard key={item.person.id} data={item.person} />);
+
+  return (
+    <div className='max-w-screen-xl mx-auto px-4'>
+      <div className='grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 dark:gap-6'>
+        {peoplesData.map((item) => (
+          <PeoplesCard key={item.person.id} data={item.person} />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default PeoplesGrid;
