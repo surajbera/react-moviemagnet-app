f09f63280db2809c678e6edf277d940838959cc1
suraj.be <suraj.be@media.net>
2023-08-02 18:59:40 +0530

    Updated 404 page

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,4 +1,28 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className='bg-white dark:bg-gray-900'>
+      <div className='py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6'>
+        <div className='mx-auto max-w-screen-sm text-center'>
+          <h1 className='mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500 dark:text-white'>
+            404
+          </h1>
+          <p className='mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white'>
+            Something's missing.
+          </p>
+          <p className='mb-4 text-lg font-light text-gray-500 dark:text-gray-400'>
+            Sorry, we can't find that page. You'll find lots to explore on the home page.{' '}
+          </p>
+          <Link
+            to='/'
+            className='inline-flex text-white bg-lightPrimary-600 dark:bg-darkPrimary-600 hover:bg-lightPrimary-800 dark:hover:bg-darkPrimary-800 focus:ring-4 focus:outline-none focus:ring-lightPrimary-300 dark:focus:ring-darkPrimary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-lightPrimary-900 my-4'
+          >
+            Back to Homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default NotFound;
