9150bebf4f099e2640d858df4bf870c8d56ecf05
suraj.be <suraj.be@media.net>
2023-08-02 14:02:37 +0530

    Done with Navbar css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const LINKS = [
@@ -13,11 +13,20 @@ const Navbar = () => {
   ];
 
   return (
-    <nav>
-      <ul>
+    <nav className='text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 mb-4 max-w-fit mx-auto my-4'>
+      <ul className='flex flex-wrap -mb-px w-full'>
         {LINKS.map((link) => (
           <li key={link.text}>
-            <Link to={link.to}>{link.text}</Link>
+            <NavLink
+              to={link.to}
+              className={({ isActive }) =>
+                isActive
+                  ? 'inline-block py-3 px-5 text-primary-600 border-b-2 border-primary-600 rounded-t-lg active dark:text-primary-500 dark:border-primary-500'
+                  : 'inline-block py-3 px-5 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+              }
+            >
+              {link.text}
+            </NavLink>
           </li>
         ))}
       </ul>
@@ -25,3 +34,8 @@ const Navbar = () => {
   );
 };
 export default Navbar;
+
+/* 
+  activeClass css: 
+    - 
+*/
