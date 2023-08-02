90b8660c401951bd0a7c8ed11938600a6a62a7e3
suraj.be <suraj.be@media.net>
2023-08-02 13:41:11 +0530

    Updated PageTitle css

--- src/components/page-title/PageTitle.jsx
+++ src/components/page-title/PageTitle.jsx
@@ -1,8 +1,10 @@
 const PageTitle = ({ title, subTitle }) => {
   return (
     <div>
-      <h1>{title}</h1>
-      <p>{subTitle}</p>
+      <h1 className='text-center text-3xl uppercase pt-6 pb-2 tracking-widest font-medium lg:text-4xl lg:pt-7'>
+        {title}
+      </h1>
+      <p className='text-center'>{subTitle}</p>
     </div>
   );
 };
