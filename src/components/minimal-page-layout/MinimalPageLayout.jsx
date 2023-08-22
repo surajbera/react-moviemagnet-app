bfea7285693ca50a38cca54658dd29439af8066f
suraj.be <suraj.be@media.net>
2023-08-22 14:00:32 +0530

    updated title

--- src/components/minimal-page-layout/MinimalPageLayout.jsx
+++ src/components/minimal-page-layout/MinimalPageLayout.jsx
@@ -7,7 +7,7 @@ import PageTitle from '../page-title/PageTitle';
 const MinimalPageLayout = () => {
   return (
     <div className='dark:bg-gray-900 dark:text-gray-300 root-wrapper bg-white text-black'>
-      <PageTitle title='Box Office' subTitle='' />
+      <PageTitle title='Movie Magnet' subTitle='' />
       <div className='pb-10 pt-16'>
         <Outlet />
       </div>
