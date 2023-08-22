f82e51c9586726ee761ae73c2a98c4d32efde964
suraj.be <suraj.be@media.net>
2023-08-22 13:01:51 +0530

    Design Update

--- src/components/peoples-grid/PeoplesGrid.jsx
+++ src/components/peoples-grid/PeoplesGrid.jsx
@@ -8,7 +8,7 @@ const PeoplesGrid = ({ peoplesData }) => {
 
   return (
     <div className='max-w-screen-xl mx-auto px-4'>
-      <div className='grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 dark:gap-6'>
+      <div className='grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 dark:gap-6'>
         {peoplesData.map((item) => (
           <PeoplesCard key={item.person.id} data={item.person} />
         ))}
--- src/components/shows-card/ShowsCard.jsx
+++ src/components/shows-card/ShowsCard.jsx
@@ -18,51 +18,53 @@ const ShowsCard = ({ data }) => {
   };
 
   return (
-    <div className='border-2 border-solid border-gray-300 rounded-lg p-2 dark:border-none dark:p-0'>
-      <div className='w-[200px] mx-auto mb-2 h-[300px] max-w-full'>
+    <div className='border-2 border-solid border-gray-300 rounded-lg p-4 dark:border-slate-600 flex flex-col'>
+      <div className='w-[200px] mx-auto mb-4 h-[300px] max-w-full'>
         <img
           src={data.image?.medium ? data.image.medium : NotFoundImg}
           alt={data.name}
           className='rounded-xl w-full h-full object-cover block'
         />
       </div>
-      <p className='mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white'>
-        {data.name}
-      </p>
-      <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
-        {data.summary ? removeHtmlTags(data.summary) : 'No Summary'}
-      </p>
-      <div className='flex justify-between'>
-        <a
-          href={`/show/${data.id}`}
-          target='_blank'
-          rel='noreferrer'
-          className='inline-flex items-center font-medium text-lightPrimary-600 hover:text-lightPrimary-800 dark:text-darkPrimary-500 dark:hover:text-darkPrimary-700'
-        >
-          Read More
-          <svg
-            className='w-2.5 h-2.5 ml-2'
-            aria-hidden='true'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 6 10'
+      <div className='flex-1 flex flex-col'>
+        <p className='mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white'>
+          {data.name}
+        </p>
+        <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+          {data.summary ? removeHtmlTags(data.summary) : 'No Summary'}
+        </p>
+        <div className='flex justify-between mt-auto'>
+          <a
+            href={`/show/${data.id}`}
+            target='_blank'
+            rel='noreferrer'
+            className='inline-flex items-center font-medium text-lightPrimary-600 hover:text-lightPrimary-800 dark:text-darkPrimary-500 dark:hover:text-darkPrimary-700'
           >
-            <path
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='m1 9 4-4-4-4'
-            />
-          </svg>
-        </a>
-        <button type='button' className={buttonClasses} onClick={() => handleStarToggle(data.id)}>
-          {starredIds.includes(data.id) ? (
-            <AiTwotoneStar size={26} color={uiMode === 'dark' ? '#fde047' : '#4f46e5'} />
-          ) : (
-            <AiOutlineStar size={26} />
-          )}
-        </button>
+            Read More
+            <svg
+              className='w-2.5 h-2.5 ml-2'
+              aria-hidden='true'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 6 10'
+            >
+              <path
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='m1 9 4-4-4-4'
+              />
+            </svg>
+          </a>
+          <button type='button' className={buttonClasses} onClick={() => handleStarToggle(data.id)}>
+            {starredIds.includes(data.id) ? (
+              <AiTwotoneStar size={26} color={uiMode === 'dark' ? '#fde047' : '#4f46e5'} />
+            ) : (
+              <AiOutlineStar size={26} />
+            )}
+          </button>
+        </div>
       </div>
     </div>
   );
